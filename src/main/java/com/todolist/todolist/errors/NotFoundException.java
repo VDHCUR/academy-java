e6ed0d84086894d0c46ae25aa9e9ef57f46b17bf
VDHCUR <justthinker@yandex.ru>
2020-10-19 01:30:06 +0700

    Changed ListNotFoundException to NotFoundException

--- src/main/java/com/todolist/todolist/errors/NotFoundException.java
+++ src/main/java/com/todolist/todolist/errors/NotFoundException.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.UUID;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ListNotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException{
 
-    public ListNotFoundException(String entityName, UUID id){
-        super(entityName + "id not found: " + id.toString());
+    public NotFoundException(String entityName, UUID id){
+        super(entityName + " id not found: " + id.toString());
     }
 
 }
